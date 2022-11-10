154590df96f6b9dc4dc00cfc1b0ed92ed431fa4f
Omer Ali <omeralis2000@gmail.com>
2022-11-10 23:46:01 +0400

    [fix scroll]

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -7,15 +7,26 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(    private viewportScroller: ViewportScroller) { }
+  positionScroll = 540;
+  visibilityBackground: boolean = true;
+  constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
   }
- 
+
   public scroll(elementId: any): void {
     this.viewportScroller.scrollToAnchor(elementId)
   }
-  
+
+  @HostListener('document:mousewheel', ['$event'])
+  scrollTop(event: any) {
+    console.log('event scroll', event.pageY)
+    if (event.pageY > this.positionScroll) {
+      this.visibilityBackground = false;
+      console.log('this.visibilityBackground', this.visibilityBackground);
+    } else {
+      this.visibilityBackground = true;
+    }
+  }
 
 }
